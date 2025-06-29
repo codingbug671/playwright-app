1efa18360038c2d6e3f29ba4d41fc5075d4c2c3e
Bushra Akram <bushra.akram999@hotmail.com>
2025-06-29 15:50:54 +0400

    added applicant type page and updated onboarding test

--- pages/ApplicantType.js
+++ pages/ApplicantType.js
@@ -3,12 +3,19 @@ class ProductCategoryPage
     constructor(page)
     {
         this.page = page
-        this.lmvaBtn = "div[data-testid='LMVA']"
+        this.fullname = "input[data-testid='input-text-customer.englishFullName']"
+        this.mobileno = "input[data-testid='input-phone-customer.mobile']"
+        this.email = "input[data-testid='input-text-customer.email']"
+        this.validateBtn = "button[data-testid='icademy-button']"
     }
 
-    async selectLightVehicleAutomaticCar()
+    async addStudentPersonalDetails(name, mobileno, email)
     {
-        await this.page.click(this.lmvaBtn)
+        await this.page.fill(this.fullname, name);
+        await this.page.fill(this.mobileno, mobileno);
+        await this.page.fill(this.email, email);
+        await this.page.locator(this.validateBtn).nth(1).click();
+
     }
 }
 
--- tests/studentonboarding.spec.js
+++ tests/studentonboarding.spec.js
@@ -4,20 +4,23 @@ const LicenseTypePage = require('../pages/LicenseTypePage');
 const ApplicantTypePage = require('../pages/ApplicantType');
 const LicenseTypeData = require('../testData/licenseType.json');
 
+
 test('Onboard a new student', async({page})=>
 {
 
     await page.goto('/');
 
     const studloginpage = new StudentLoginPage(page);
-    const licenseTypePage = new LicenseTypePage(page);
+    const licensetypepage = new LicenseTypePage(page);
     const product = LicenseTypeData.licenseType.product;
     const category = LicenseTypeData.licenseType.category;
+    const applicanttypepage = new ApplicantTypePage(page);
 
   
     await studloginpage.createNewAccount();
-    await licenseTypePage.selectProduct(product);
-    await licenseTypePage.selectProductCategory(category);
+    await licensetypepage.selectProduct(product);
+    await licensetypepage.selectProductCategory(category);
+    await applicanttypepage.addStudentPersonalDetails("testuser","971504097652","abc@gmail.com");
 
 
 
